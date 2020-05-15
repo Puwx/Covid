e06c83be8acbb74d0017a4215c67417e9ae16aa4
Matthew Kemper <37680606+Puwx@users.noreply.github.com>
2020-05-14 20:15:43 -0600

    Add files via upload

---
+++ GetCovidData.py
@@ -0,0 +1,33 @@
+import re
+import os
+import time
+import requests 
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+def replaceChars(text):
+    noSym = re.sub(r"[\xa0\s]","_",text)
+    return re.sub(r"\/","Per",noSym)
+
+
+raw = requests.get(r"https://www.worldometers.info/coronavirus/").text
+soup = bs(raw)
+
+table = soup.find('table',{'id':'main_table_countries_today'})
+header = table.find('thead')
+
+columns = [replaceChars(h.text) for h in header.find_all('th')]
+
+body = table.find_all('tbody')[0]
+
+dataRows = []
+for row in body.find_all('tr'):
+    if not row.get('style',None):
+        row = [td.text for td in row.find_all('td')]
+        dataRows.append(row)
+
+os.chdir(r"C:\Scripts\Covid")
+
+df = pd.DataFrame(dataRows,columns=columns)
+df.to_csv('CovidData_{}.csv'.format(time.strftime("%Y%m%d")))
+print('Done')
